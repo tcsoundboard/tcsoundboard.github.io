f3c4d2024e702fd26d45e27db2a10e5b15f9c2bb
lscorrcho <l.scorrcho@gmail.com>
2020-02-20 20:24:20 -0500

    new sounds

--- js/index.js
+++ js/index.js
@@ -88,7 +88,7 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 	  	sounds: [
-            { id: 1, soundName: 'TIME CRISIS INTRO (DROP)', soundURL: './sounds/tcintro.mp3', isPlaying: false },
+	  	 	{ id: 1, soundName: 'TIME CRISIS INTRO (DROP)', soundURL: './sounds/tcintro.mp3', isPlaying: false },
 	  	 	{ id: 2, soundName: '8 MINUTE CAPE COD (DROP)', soundURL: './sounds/8minutecapecod.mp3', isPlaying: false },
 	  	 	{ id: 3, soundName: 'SWEET CHILI HEAT® WORLD PREMIERE (DROP)', soundURL: './sounds/tcsweetchilipremiere.mp3', isPlaying: false },
 	  	 	{ id: 4, soundName: 'TASTEFUL PALETTE OF THE 1970\'S (DROP)', soundURL: './sounds/tastefulpalette.mp3', isPlaying: false },
@@ -103,11 +103,11 @@ class App extends React.Component {
 			{ id: 13, soundName: 'ROUGH STUFF (DROP)', soundURL: './sounds/roughstuffdrop.mp3', isPlaying: false },
 			{ id: 14, soundName: 'NARCISSISM OF SMALL DIFFERENCES (DROP)', soundURL: './sounds/narcissismdrop.mp3', isPlaying: false },
 			{ id: 15, soundName: 'FORM IS EMPTINESS (DROP)', soundURL: './sounds/formisemptinessdrop.mp3', isPlaying: false },
-            { id: 16, soundName: 'NOW I AM BECOME DEATH (DROP)', soundURL: './sounds/nowiambecomedeath.mp3', isPlaying: false, isNew: true },
-            { id: 17, soundName: 'NEW YORK NEWS (DROP)', soundURL: './sounds/newyorknews.mp3', isPlaying: false, isNew: true },
-            { id: 18, soundName: 'TIME CRISIS ENEMIES (DROP)', soundURL: './sounds/tcenemies.mp3', isPlaying: false, isNew: true },
-            { id: 19, soundName: 'THIS IS YOUR SONG (DROP)', soundURL: './sounds/thisisyoursong.mp3', isPlaying: false, isNew: true },
-            { id: 20, soundName: 'LOVE, I DON\'T GET ENOUGH OF IT (DROP)', soundURL: './sounds/loveidontgetenoughofit.mp3', isPlaying: false, isNew: true },
+			{ id: 16, soundName: 'NOW I AM BECOME DEATH (DROP)', soundURL: './sounds/nowiambecomedeath.mp3', isPlaying: false },
+			{ id: 17, soundName: 'NEW YORK NEWS (DROP)', soundURL: './sounds/newyorknews.mp3', isPlaying: false },
+			{ id: 18, soundName: 'TIME CRISIS ENEMIES (DROP)', soundURL: './sounds/tcenemies.mp3', isPlaying: false },
+			{ id: 19, soundName: 'THIS IS YOUR SONG (DROP)', soundURL: './sounds/thisisyoursong.mp3', isPlaying: false },
+			{ id: 20, soundName: 'LOVE, I DON\'T GET ENOUGH OF IT (DROP)', soundURL: './sounds/loveidontgetenoughofit.mp3', isPlaying: false },
 			{ id: 21, soundName: 'THIS SHOW RULES', soundURL: './sounds/thisshowrules.mp3', isPlaying: false },
 			{ id: 22, soundName: 'ROUGH STUFF FOLKS', soundURL: './sounds/roughstufffolks.mp3', isPlaying: false },
 			{ id: 23, soundName: 'SEINFELD NUMBER CRUNCH', soundURL: './sounds/seinfeldnumbercrunch.mp3', isPlaying: false },
@@ -133,13 +133,15 @@ class App extends React.Component {
 			{ id: 43, soundName: '#THEBIG3 THAT MAKEMEME', soundURL: './sounds/thebig3thatmakememe.mp3', isPlaying: false },
 			{ id: 44, soundName: 'WANGS', soundURL: './sounds/wangs.mp3', isPlaying: false },
 			{ id: 45, soundName: 'NO FAP SUMMER', soundURL: './sounds/nofapsummer.mp3', isPlaying: false },
-            { id: 46, soundName: 'KIND VIBE', soundURL: './sounds/kindvibe.mp3', isPlaying: false, isNew: true },
-            { id: 47, soundName: 'BUT THIS WAS A FANTASY', soundURL: './sounds/thiswasafantasy.mp3', isPlaying: false, isNew: true },
-            { id: 48, soundName: 'WHAT\'S WRONG WITH BEING FAT', soundURL: './sounds/whatswrongwithbeingfat.mp3', isPlaying: false, isNew: true },
-            { id: 49, soundName: 'AS A MUSIC INDUSTRY PROFESS- IONAL', soundURL: './sounds/musicindustryprofessional.mp3', isPlaying: false, isNew: true },
-            { id: 50, soundName: 'WHOA, HOLD UP GIRL...', soundURL: './sounds/whoaholdupgirl.mp3', isPlaying: false, isNew: true },
-            { id: 51, soundName: 'AND IT\'S EASY TO SEE', soundURL: './sounds/anditseasytosee.mp3', isPlaying: false, isNew: true },
-            { id: 52, soundName: 'DOWN MEXICO WAY', soundURL: './sounds/downmexicoway.mp3', isPlaying: false, isNew: true },
+			{ id: 46, soundName: 'KIND VIBE', soundURL: './sounds/kindvibe.mp3', isPlaying: false },
+			{ id: 47, soundName: 'BUT THIS WAS A FANTASY', soundURL: './sounds/thiswasafantasy.mp3', isPlaying: false },
+			{ id: 48, soundName: 'WHAT\'S WRONG WITH BEING FAT?', soundURL: './sounds/whatswrongwithbeingfat.mp3', isPlaying: false },
+			{ id: 49, soundName: 'AS A MUSIC INDUSTRY PROFESS- IONAL', soundURL: './sounds/musicindustryprofessional.mp3', isPlaying: false },
+			{ id: 50, soundName: 'WHOA, HOLD UP GIRL...', soundURL: './sounds/whoaholdupgirl.mp3', isPlaying: false },
+			{ id: 51, soundName: 'AND IT\'S EASY TO SEE', soundURL: './sounds/anditseasytosee.mp3', isPlaying: false },
+			{ id: 52, soundName: 'DOWN MEXICO WAY', soundURL: './sounds/downmexicoway.mp3', isPlaying: false },
+			{ id: 53, soundName: 'FRICKIN RANDOM', soundURL: './sounds/frickinrandom.mp3', isPlaying: false, isNew: true },
+			{ id: 54, soundName: 'WHAT ABOUT VAMPIRE, MAN?', soundURL: './sounds/whataboutvampireman.mp3', isPlaying: false, isNew: true },
 	  	]};
 
 
